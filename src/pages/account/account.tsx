4b579840ab619cf0a2bce1454c7a94b06a34c322
hunghaa <hunghaa2906@gmail.com>
2025-04-02 14:07:25 +0700

    fix build and setup prettier

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -1,94 +1,94 @@
-import { Icon } from "@iconify/react";
-import { useLogout } from "apiRequest/auth";
-import images from "assets/images";
-import { HeaderSub } from "components/header-sub"
-import React from "react"
-import { useTranslation } from "react-i18next";
-import { useLoginWithZalo } from "services/loginWithZalo";
-import { openPermissionSettingApp } from "services/zalo";
-import { useStoreApp } from "store/store";
-import { Box, List, Page, useNavigate } from "zmp-ui"
+import { Icon } from '@iconify/react'
+import { useLogout } from 'apiRequest/auth'
+import images from 'assets/images'
+import { HeaderSub } from 'components/header-sub'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useLoginWithZalo } from 'services/loginWithZalo'
+import { openPermissionSettingApp } from 'services/zalo'
+import { useStoreApp } from 'store/store'
+import { Box, List, Page, useNavigate } from 'zmp-ui'
 
 const AccountPage: React.FC = () => {
+  const { Item } = List
 
-    const { Item } = List;
+  const navigate = useNavigate()
+  const { loginWithZalo } = useLoginWithZalo()
+  const { account } = useStoreApp()
+  const logout = useLogout()
+  const { t: tCommon } = useTranslation('common')
+  const { t: tAccount } = useTranslation('account')
 
-    const navigate = useNavigate()
-    const { loginWithZalo } = useLoginWithZalo();
-    const { account } = useStoreApp();
-    const logout = useLogout();
-    const { t: tCommon } = useTranslation("common");
-    const { t: tAccount } = useTranslation("account");
-
-    return (
-        <Page className="relative flex-1 flex flex-col bg-white pb-[66px]" style={{ backgroundColor: '#f5f6f7' }}>
+  return (
+    <Page
+      className="relative flex-1 flex flex-col bg-white pb-[66px]"
+      style={{ backgroundColor: '#f5f6f7' }}
+    >
+      <Box>
+        <HeaderSub
+          title={tCommon('account')}
+          onBackClick={() => navigate('/')}
+        />
+        <Box>
+          {account ? (
+            <>
+              <Box m={4}>
+                <List className="bg-white rounded-lg">
+                  <Item
+                    onClick={() => navigate('/profile-account')}
+                    title={tAccount('information')}
+                    prefix={<img src={images.resume} width={30} />}
+                    suffix={<Icon fontSize={20} icon="formkit:right" />}
+                  />
+                  <Item
+                    onClick={() => navigate('/change-password')}
+                    title={tAccount('change-pw')}
+                    prefix={<img src={images.changePw} width={30} />}
+                    suffix={<Icon fontSize={20} icon="formkit:right" />}
+                  />
+                  <Item
+                    onClick={logout}
+                    title={tAccount('logout')}
+                    prefix={<img src={images.logout} width={30} />}
+                    suffix={<Icon fontSize={20} icon="formkit:right" />}
+                  />
+                </List>
+              </Box>
+            </>
+          ) : (
             <Box>
-                <HeaderSub title={tCommon("account")} onBackClick={() => navigate('/')} />
-                <Box>
-
-                    {
-                        account ?
-                            <>
-                                <Box m={4}>
-                                    <List className="bg-white rounded-lg">
-                                        <Item
-                                            onClick={() => navigate('/profile-account')}
-                                            title={tAccount('information')}
-                                            prefix={<img src={images.resume} width={30} />}
-                                            suffix={<Icon fontSize={20} icon="formkit:right" />}
-                                        />
-                                        <Item
-                                            onClick={() => navigate('/change-password')}
-                                            title={tAccount('change-pw')}
-                                            prefix={<img src={images.changePw} width={30} />}
-                                            suffix={<Icon fontSize={20} icon="formkit:right" />}
-                                        />
-                                        <Item
-                                            onClick={logout}
-                                            title={tAccount('logout')}
-                                            prefix={<img src={images.logout} width={30} />}
-                                            suffix={<Icon fontSize={20} icon="formkit:right" />}
-                                        />
-                                    </List>
-                                </Box>
-                            </>
-                            :
-                            <Box>
-                                <Box m={4}>
-                                    <List className="bg-white rounded-lg">
-                                        <Item
-                                            onClick={() => loginWithZalo()}
-                                            title={tAccount('login-zalo')}
-                                            prefix={<img src={images.zalo} width={30} />}
-                                            suffix={<Icon fontSize={20} icon="formkit:right" />}
-                                        />
-                                        <Item
-                                            onClick={() => navigate('/login')}
-                                            title={tAccount('login-account')}
-                                            prefix={<img src={images.login} width={30} />}
-                                            suffix={<Icon fontSize={20} icon="formkit:right" />}
-                                        />
-                                    </List>
-                                </Box>
-
-
-                            </Box>
-
-                    }
-                    <Box m={4}>
-                        <List className="bg-white rounded-lg">
-                            <Item
-                                onClick={() => navigate('/register')}
-                                title={tAccount('register')}
-                                prefix={<img src={images.signup} width={30} />}
-                                suffix={<Icon fontSize={20} icon="formkit:right" />}
-                            />
-                        </List>
-                    </Box>
-                </Box>
+              <Box m={4}>
+                <List className="bg-white rounded-lg">
+                  <Item
+                    onClick={() => loginWithZalo()}
+                    title={tAccount('login-zalo')}
+                    prefix={<img src={images.zalo} width={30} />}
+                    suffix={<Icon fontSize={20} icon="formkit:right" />}
+                  />
+                  <Item
+                    onClick={() => navigate('/login')}
+                    title={tAccount('login-account')}
+                    prefix={<img src={images.login} width={30} />}
+                    suffix={<Icon fontSize={20} icon="formkit:right" />}
+                  />
+                </List>
+              </Box>
             </Box>
-        </Page>
-    )
+          )}
+          <Box m={4}>
+            <List className="bg-white rounded-lg">
+              <Item
+                onClick={() => navigate('/register')}
+                title={tAccount('register')}
+                prefix={<img src={images.signup} width={30} />}
+                suffix={<Icon fontSize={20} icon="formkit:right" />}
+              />
+            </List>
+          </Box>
+        </Box>
+      </Box>
+    </Page>
+  )
 }
 
 export default AccountPage
--- vite.config.mts
+++ vite.config.mts
@@ -2,12 +2,31 @@ import { defineConfig } from "vite";
 import zaloMiniApp from "zmp-vite-plugin";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vitejs.dev/config/
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export default () => {
   return defineConfig({
     root: "./src",
     base: "",
     plugins: [zaloMiniApp(), react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "css": path.resolve(__dirname, "src/css"), 
+        "components": path.resolve(__dirname, "src/components"), 
+        "pages": path.resolve(__dirname, "src/pages"), 
+        "store": path.resolve(__dirname, "src/store"), 
+        "services": path.resolve(__dirname, "src/services"), 
+        "assets": path.resolve(__dirname, "src/assets"), 
+        "apiRequest": path.resolve(__dirname, "src/apiRequest"), 
+        "utils": path.resolve(__dirname, "src/utils"), 
+        "constants": path.resolve(__dirname, "src/constants"), 
+        "locales": path.resolve(__dirname, "src/locales"), 
+        "envConfig": path.resolve(__dirname, "src/envConfig"), 
+      },
+    },
   });
 };
