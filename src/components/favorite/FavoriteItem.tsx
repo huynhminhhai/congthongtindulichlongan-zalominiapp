e5223dbb113271228299187c18b53cdb14028c08
hunghaa <hunghaa2906@gmail.com>
2025-04-04 09:00:22 +0700

    add icon heart to favorite item

--- src/components/favorite/FavoriteItem.tsx
+++ src/components/favorite/FavoriteItem.tsx
@@ -1,26 +1,51 @@
 import { Icon } from '@iconify/react'
 import React from 'react'
 import { Box } from 'zmp-ui'
-
-const FavoriteItem: React.FC<any> = ({ data }) => {
-
-    return (
-        <Box className='border-[1px] rounded-lg'>
-            <img className='w-full h-[210px] object-cover rounded-lg' src={data.img} alt={data.title} />
-            <Box px={3} py={4}>
-                <h3 className='text-[16px] font-bold text-[#355933] line-clamp-1 mb-2'>{data.title}</h3>
-                <ul className='flex flex-col gap-1 text-[14px] leading-[18px] font-medium'>
-                    <li className='flex items-center gap-1'>
-                        <Icon fontSize={24} icon='codicon:tag' />
-                        {data.category}
-                    </li>
-                </ul>
-                <div className="actions flex items-center justify-end" onClick={() => console.log('remove form favorite list')}>
+import styles from './index.module.scss'
+interface IFavoriteItem {
+  image: string
+  title: string
+  category: string
+  isFavorite?: boolean
+  onCancel?: () => void
+}
+const FavoriteItem: React.FC<IFavoriteItem> = ({
+  image,
+  title,
+  category,
+  isFavorite = false,
+  onCancel,
+}) => {
+  return (
+    <Box className={styles.favoriteItem}>
+      <div className={styles.toggleFavorite}>
+        {isFavorite ? (
+          <Icon icon="line-md:heart-filled" className={styles.active} />
+        ) : (
+          <Icon icon="mdi-light:heart" />
+        )}
+      </div>
+      <img
+        className="w-full h-[210px] object-cover rounded-lg"
+        src={image}
+        alt={title}
+      />
+      <Box px={3} py={4}>
+        <h3 className="text-[18px] font-bold text-[#355933] line-clamp-2 mb-2">
+          {title}
+        </h3>
+        <ul className="flex flex-col gap-1 text-[14px] leading-[18px] font-medium">
+          <li className="flex items-center gap-1">
+            <Icon fontSize={18} icon="codicon:tag" />
+            {category}
+          </li>
+        </ul>
+        {/* <div className="actions flex items-center justify-end" onClick={() => console.log('remove form favorite list')}>
                     <button className="btn-bookmark active"><img className='w-[30px]' src="https://cdn-icons-png.flaticon.com/128/6460/6460112.png" alt="mục yêu thích" /></button>
-                </div>
-            </Box>
-        </Box>
-    )
+                </div> */}
+      </Box>
+    </Box>
+  )
 }
 
 export default FavoriteItem
--- src/pages/favorite/favorite.tsx
+++ src/pages/favorite/favorite.tsx
@@ -7,81 +7,75 @@ import { useTranslation } from 'react-i18next'
 import { Box, Input, Page, Select } from 'zmp-ui'
 
 export const favorite = [
-    {
-        "title": "Nhà Trăm Cột",
-        "img": "https://ik.imagekit.io/tvlk/blog/2023/11/nha-tram-cot-cover.jpg",
-        "category": "Điểm đến nổi bật",
-        "favoriteIcon": "https://cdn-icons-png.flaticon.com/128/6460/6460112.png",
-        "infoIcon": "https://cdn-icons-png.flaticon.com/128/1620/1620735.png"
-    },
-    {
-        "title": "Tour 2 ngày 1 đêm- khách nội địa",
-        "img": "https://scontent.iocvnpt.com/resources/portal//Images/LAN/trietnm.lan/duc_hoa/lang_co_phuoc_loc_tho/cropper_637189358343630424.jpg",
-        "category": "Tour du lịch",
-        "favoriteIcon": "https://cdn-icons-png.flaticon.com/128/6460/6460112.png",
-        "infoIcon": "https://cdn-icons-png.flaticon.com/128/1620/1620735.png"
-    },
-    {
-        "title": "Cánh đồng bất tận",
-        "img": "https://scontent.iocvnpt.com/resources/portal//Images/LAN/toanlm.lan/hinh_diem/khudulichcanhdongbattan_263205966.jpg",
-        "category": "Điểm du lịch",
-        "favoriteIcon": "https://cdn-icons-png.flaticon.com/128/6460/6460112.png",
-        "infoIcon": "https://cdn-icons-png.flaticon.com/128/1620/1620735.png"
-    },
-    {
-        "title": "Lẩu mắm Long An",
-        "img": "https://cdn.buffetposeidon.com/app/media/uploaded-files/110724-lau-mam-mien-tay-cung-buffet-poseidon-1-1.jpg",
-        "category": "Đặc sản",
-        "favoriteIcon": "https://cdn-icons-png.flaticon.com/128/6460/6460112.png",
-        "infoIcon": "https://cdn-icons-png.flaticon.com/128/1620/1620735.png"
-    }
-];
-
+  {
+    title: 'Nhà Trăm Cột',
+    img: 'https://ik.imagekit.io/tvlk/blog/2023/11/nha-tram-cot-cover.jpg',
+    category: 'Điểm đến nổi bật',
+    favoriteIcon: 'https://cdn-icons-png.flaticon.com/128/6460/6460112.png',
+    infoIcon: 'https://cdn-icons-png.flaticon.com/128/1620/1620735.png',
+  },
+  {
+    title: 'Tour 2 ngày 1 đêm- khách nội địa',
+    img: 'https://scontent.iocvnpt.com/resources/portal//Images/LAN/trietnm.lan/duc_hoa/lang_co_phuoc_loc_tho/cropper_637189358343630424.jpg',
+    category: 'Tour du lịch',
+    favoriteIcon: 'https://cdn-icons-png.flaticon.com/128/6460/6460112.png',
+    infoIcon: 'https://cdn-icons-png.flaticon.com/128/1620/1620735.png',
+  },
+  {
+    title: 'Cánh đồng bất tận',
+    img: 'https://scontent.iocvnpt.com/resources/portal//Images/LAN/toanlm.lan/hinh_diem/khudulichcanhdongbattan_263205966.jpg',
+    category: 'Điểm du lịch',
+    favoriteIcon: 'https://cdn-icons-png.flaticon.com/128/6460/6460112.png',
+    infoIcon: 'https://cdn-icons-png.flaticon.com/128/1620/1620735.png',
+  },
+  {
+    title: 'Lẩu mắm Long An',
+    img: 'https://cdn.buffetposeidon.com/app/media/uploaded-files/110724-lau-mam-mien-tay-cung-buffet-poseidon-1-1.jpg',
+    category: 'Đặc sản',
+    favoriteIcon: 'https://cdn-icons-png.flaticon.com/128/6460/6460112.png',
+    infoIcon: 'https://cdn-icons-png.flaticon.com/128/1620/1620735.png',
+  },
+]
 
 const FavoritePage = () => {
+  const { Option } = Select
+  const { t } = useTranslation('common')
 
-    const { Option } = Select
-    const { t } = useTranslation("common");
-
-    return (
-        <Page className="relative flex-1 flex flex-col bg-white">
-            <Box>
-                <HeaderSub title={t('favorites')} />
-                <Box pb={4}>
-                    <Box>
-                        <FilterBar
-                            showAddButton={false}
-                            searchComponent={
-                                <Input.Search
-                                    placeholder={t('searching')}
-                                    value={''}
-                                />
-                            }
-                        >
-                            <div className="col-span-12">
-                                <Select
-                                    placeholder="Danh mục"
-                                    closeOnSelect
-                                >
-                                    <Option title={'Tất cả'} value={0} />
-                                    <Option title={'Nổi tiếng'} value={1} />
-                                </Select>
-                            </div>
-                        </FilterBar>
-                    </Box>
-                    <Box px={4}>
-                        {
-                            favorite.map((item, index) => (
-                                <Box mb={6} key={index}>
-                                    <FavoriteItem data={item} />
-                                </Box>
-                            ))
-                        }
-                    </Box>
-                </Box>
-            </Box>
-        </Page>
-    )
+  return (
+    <Page className="relative flex-1 flex flex-col bg-white pb-[62px]">
+      <Box>
+        <HeaderSub title={t('favorites')} />
+        <Box>
+          <Box>
+            <FilterBar
+              showAddButton={false}
+              searchComponent={
+                <Input.Search placeholder={t('searching')} value={''} />
+              }
+            >
+              <div className="col-span-12">
+                <Select placeholder="Danh mục" closeOnSelect>
+                  <Option title={'Tất cả'} value={0} />
+                  <Option title={'Nổi tiếng'} value={1} />
+                </Select>
+              </div>
+            </FilterBar>
+          </Box>
+          <Box px={4}>
+            {favorite.map((item, index) => (
+              <Box mb={6} key={index}>
+                <FavoriteItem
+                  image={item.img}
+                  category={item.category}
+                  title={item.title}
+                />
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </Page>
+  )
 }
 
 export default FavoritePage
